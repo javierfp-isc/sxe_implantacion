cc1d32f7fea8818f157783c3972235365b79b440
Javier Fernández Peón <javierfp@iessanclemente.net>
2020-04-29 18:06:12 +0200

    cambio db_host

--- odoo_1container/odoo/entrypoint.sh
+++ odoo_1container/odoo/entrypoint.sh
@@ -14,6 +14,9 @@ chown odoo /var/log/odoo/odoo.log
 #Crea el usuario odoo en postgres
 su - postgres -c "createuser --createdb $ODOOUSER" && su - postgres -c "psql -c \"alter role $ODOOUSER with password '$ODOOPASS'\""
 
+#Escribe en /etc/hosts para que resuelva postgres al localhost
+echo "127.0.0.1 postgres" >> /etc/hosts
+
 #Arranca odoo (while: la primera vez no arranca bien)
 while [[ ! $(service odoo.sh start) ]];do continue;done
 
--- odoo_despliegue/odoo/entrypoint.sh
+++ odoo_despliegue/odoo/entrypoint.sh
@@ -7,6 +7,9 @@ service postgresql start
 #Crea el usuario odoo en postgres
 su - postgres -c "createuser --createdb $ODOOUSER" && su - postgres -c "psql -c \"alter role $ODOOUSER with password '$ODOOPASS'\""
 
+#Escribe en /etc/hosts para que resuelva postgres al localhost
+echo "127.0.0.1 postgres" >> /etc/hosts
+
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
 
