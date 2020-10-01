a20118b0496767a21e0ea0ef3b84ba0e73833e43
Javier Fernández Peón <javierfp@iessanclemente.net>
2020-10-01 21:39:09 +0200

    while en arranque nginx

--- nginx_odoo_ssl/nginx/entrypoint.sh
+++ nginx_odoo_ssl/nginx/entrypoint.sh
@@ -3,8 +3,8 @@
 #Arrancamos ssh
 service ssh start
 
-#Arranca nginx (while: la primera vez no arranca bien)
-while [[ ! $(service nginx start) ]];do continue;done
+#Arranca nginx 
+/etc/init.d/nginx start
 
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
