5a6a9a8c661275ff178be9f830c833a22c2e8f90
Javier Fernández Peón <javierfp@iessanclemente.net>
2020-10-01 21:43:30 +0200

    arranque nginx

--- nginx_odoo_ssl/nginx/entrypoint.sh
+++ nginx_odoo_ssl/nginx/entrypoint.sh
@@ -4,7 +4,7 @@
 service ssh start
 
 #Arranca nginx 
-/etc/init.d/nginx start
+service nginx start
 
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
