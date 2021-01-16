81b63f073a8147b255fe03c7ccb3765b7a556426
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-01-16 19:13:31 +0100

    Escenario base para múltiples versiones de Odoo

---
+++ odoo_base/odoo/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Sustituimos las variables de entorno en el odoo.conf
+sed -i "s/db_host = DBHOST/db_host = $DBHOST/" $ODOOHOME/odoo.conf
+sed -i "s/db_user = DBUSER/db_user = $DBUSER/" $ODOOHOME/odoo.conf
+sed -i "s/db_password = DBPASS/db_password = $DBPASS/" $ODOOHOME/odoo.conf
+sed -i "s/admin_passwd = ADMINPASS/admin_passwd = $ADMINPASS/" $ODOOHOME/odoo.conf
+sed -i "s%logfile = LOGFILE%logfile = $LOGFILE%" $ODOOHOME/odoo.conf
+
+#Arrancamos servicios
+service postgresql start
+
+#Crea el usuario odoo en postgres
+su - postgres -c "createuser --createdb $DBUSER" && su - postgres -c "psql -c \"alter role $DBUSER with password '$DBPASS'\""
+
+#Escribe en /etc/hosts para que resuelva postgres al localhost
+echo "127.0.0.1 postgres" >> /etc/hosts
+
+#Uso exec para lanzar un proceso independiente de bucle infinito
+exec bash -c "while true;do sleep 10;done"
+
