ab77203070c61e0007f7d8d3ec76f12aa72220b4
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-07-23 16:19:55 +0200

    Modificando el despliegue

--- odoo_despliegue/odoo/entrypoint.sh
+++ odoo_despliegue/odoo/entrypoint.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-#Crea el usuario para odoo
-if [[ ! $(id $ODOOUSER) ]]
-then
-    useradd -m -d /opt/$ODOOUSER -U -s /bin/bash $ODOOUSER
-    chown $ODOOUSER:$ODOOUSER /opt/$ODOOUSER
-    echo "$ODOOUSER:$ODOOPASS" | chpasswd
-fi
+#Habilitar acceso desde host remotos a postgres
+[[ $(grep "^host all all 0.0.0.0/0 md5" /etc/postgresql/11/main/pg_hba.conf) ]] || echo "host all all 0.0.0.0/0 md5" >> /etc/postgresql/11/main/pg_hba.conf
+[[ $(grep "^listen_addresses='*'" /etc/postgresql/11/main/postgresql.conf) ]] || echo "listen_addresses='*'" >> /etc/postgresql/11/main/postgresql.conf
 
 #Arrancamos servicios
-service ssh start
 service postgresql start
 
 #Crea el usuario odoo en postgres
-su - postgres -c "createuser --createdb $ODOOUSER" && su - postgres -c "psql -c \"alter role $ODOOUSER with password '$ODOOPASS'\""
+su - postgres -c "createuser --createdb $DBUSER" && su - postgres -c "psql -c \"alter role $DBUSER with password '$DBPASS'\""
 
 #Escribe en /etc/hosts para que resuelva postgres al localhost
 echo "127.0.0.1 postgres" >> /etc/hosts
