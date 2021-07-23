0e001bc5f8a4054f93176f59a562d384651cc0ce
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-07-23 17:45:26 +0200

    OCBDIR para 14

--- nginx_odoo_ssl/odoo/entrypoint.sh
+++ nginx_odoo_ssl/odoo/entrypoint.sh
@@ -6,6 +6,9 @@ sed -i "s/db_user = DBUSER/db_user = $DBUSER/" /opt/odoo/odoo.conf
 sed -i "s/db_password = DBPASS/db_password = $DBPASS/" /opt/odoo/odoo.conf
 sed -i "s/admin_passwd = ADMINPASS/admin_passwd = $ADMINPASS/" /opt/odoo/odoo.conf
 sed -i "s%logfile = LOGFILE%logfile = $LOGFILE%" /opt/odoo/odoo.conf
+sed -i "s/OCBDIR/$OCBDIR/" /opt/odoo/odoo.conf
+#Sustituimos las variables de entorno en el odoo.sh
+sed -i "s/OCBDIR/$OCBDIR/" /etc/init.d/odoo.sh
 
 #Arrancamos servicios
 service ssh start
