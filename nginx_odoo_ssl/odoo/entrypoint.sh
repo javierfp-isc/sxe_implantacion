72b7c28eb12f3446794ae20503e505bc8a1db0f1
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-11-24 11:47:23 +0100

    permisos

--- nginx_odoo_ssl/odoo/entrypoint.sh
+++ nginx_odoo_ssl/odoo/entrypoint.sh
@@ -10,15 +10,16 @@ sed -i "s/OCBDIR/$OCBDIR/" /opt/odoo/odoo.conf
 #Sustituimos las variables de entorno en el odoo.sh
 sed -i "s/OCBDIR/$OCBDIR/" /etc/init.d/odoo.sh
 
-#Arrancamos servicios
-service ssh start
-
 #Creamos el archivo de log y cambiamos propietario
+mkdir -p $(dirname $LOGFILE)
 touch $LOGFILE
-chown $ODOOUSER $LOGFILE
+chown -R $ODOOUSER:$ODOOUSER $(dirname $LOGFILE)
 
 #Arranca odoo (while: la primera vez no arranca bien)
 while [[ ! $(service odoo.sh start) ]];do continue;done
+
+#Cambiamos propietario del filestore
+[ $(stat -c "%G" $LOCALFS) == "$ODOOUSER" ] || chown -R $ODOOUSER:$ODOOUSER $LOCALFS
 
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
