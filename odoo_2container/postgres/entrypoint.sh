ada59372e1cb1e27d0d463e76086cf85f03b6ae7
Javier Fernández Peón <javierfp@iessanclemente.net>
2022-11-14 17:22:07 +0100

    Fix postgres 13

--- odoo_2container/postgres/entrypoint.sh
+++ odoo_2container/postgres/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#Habilitar acceso desde host remotos
-[[ $(grep "^host all all 0.0.0.0/0 md5" /etc/postgresql/13/main/pg_hba.conf) ]] || echo "host all all 0.0.0.0/0 md5" >> /etc/postgresql/13/main/pg_hba.conf
-[[ $(grep "^listen_addresses='*'" /etc/postgresql/13/main/postgresql.conf) ]] || echo "listen_addresses='*'" >> /etc/postgresql/13/main/postgresql.conf
-
 #Arrancamos servicios
 service postgresql start
 
@@ -12,4 +8,3 @@ su - postgres -c "createuser --createdb $DBUSER" && su - postgres -c "psql -c \"
 
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
-
