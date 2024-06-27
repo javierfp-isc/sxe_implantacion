b818f31030e2b72c4edab113c74dbf6ab8d42624
javierfp <javierfp@mentorl01.sanclemente.local>
2024-06-27 13:16:14 +0200

    cambios 17

--- nginx_odoo_ssl/nginx/entrypoint.sh
+++ nginx_odoo_ssl/nginx/entrypoint.sh
@@ -4,5 +4,5 @@
 service nginx start
 
 #Uso exec para lanzar un proceso independiente de bucle infinito
-exec bash -c "while true;do sleep 10;done"
+exec bash -c "while true;do sleep 1000;done"
 
--- nginx_odoo_ssl/postgres/entrypoint.sh
+++ nginx_odoo_ssl/postgres/entrypoint.sh
@@ -7,5 +7,5 @@ service postgresql start
 su - postgres -c "createuser --createdb $DBUSER" && su - postgres -c "psql -c \"alter role $DBUSER with password '$DBPASS'\""
 
 #Uso exec para lanzar un proceso independiente de bucle infinito
-exec bash -c "while true;do sleep 10;done"
+exec bash -c "while true;do sleep 1000;done"
 
